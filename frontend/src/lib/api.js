8cca0e36d8a33f81ef454bfe13c467e3efcbd4f9
vsreddyh <shouryanreddyh@gmail.com>
2026-02-21 12:42:01 +0530

    fixed extra / issue

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,4 @@
-const API = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API = (import.meta.env.VITE_API_URL || 'http://localhost:5000').replace(/\/+$/, '');
 
 export const getToken = () => localStorage.getItem('foxhole_token');
 export const setToken = (t) => localStorage.setItem('foxhole_token', t);
